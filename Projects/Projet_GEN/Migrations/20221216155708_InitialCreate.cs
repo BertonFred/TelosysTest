350944319bca8a741289500cecfab62544458d71
Fred Berton <frederic.berton@capgemini.com>
2022-12-16 17:16:27 +0100

    good

---
+++ Projects/Projet_GEN/Migrations/20221216155708_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace wsRestTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    oid = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.oid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    oid = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    comment = table.Column<string>(type: "TEXT", nullable: false),
+                    Postoid = table.Column<long>(name: "Post_oid", type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.oid);
+                    table.ForeignKey(
+                        name: "FK_Comments_Posts_Post_oid",
+                        column: x => x.Postoid,
+                        principalTable: "Posts",
+                        principalColumn: "oid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "oid", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1L, "Description 1", "Title 1" },
+                    { 2L, "Description 2", "Title 2" },
+                    { 3L, "Description 3", "Title 3" },
+                    { 4L, "Description 4", "Title 4" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_Post_oid",
+                table: "Comments",
+                column: "Post_oid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
--- Projects/Projet_GEN/README.md
+++ Projects/Projet_GEN/README.md
@@ -8,3 +8,21 @@ dotnet-ef dotnet add package Microsoft.EntityFrameworkCore.Design
 dotnet ef migrations add InitialCreate
 
 dotnet ef database update
+
+Recreation
+del .\Migrations\*.cs
+Drop-Database
+Add-Migration InitialCreate
+Update-Database
+
+
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // initialisation des données 
+            modelBuilder.Entity<Post>().HasData(new { oid = 1L, Title ="Title 1",Description="Description 1"});
+            modelBuilder.Entity<Post>().HasData(new { oid = 2L, Title = "Title 2", Description = "Description 2" });
+            modelBuilder.Entity<Post>().HasData(new { oid = 3L, Title = "Title 3", Description = "Description 3" });
+            modelBuilder.Entity<Post>().HasData(new { oid = 4L, Title = "Title 4", Description = "Description 4" });
+        }
