19f61fd2c100d56a8b1090e2de9ab79c80f6e709
Fred Berton <berton.Frederic@gmail.Com>
2023-02-18 18:47:10 +0100

    modification du templaet pour avoir le projets complet automatiquement

---
+++ Projects/BackEnd_GEN/Migrations/20230218174119_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wsRestTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    oid = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.oid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    oid = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    comment = table.Column<string>(type: "TEXT", nullable: false),
+                    Post_oid = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.oid);
+                    table.ForeignKey(
+                        name: "FK_Comments_Posts_Post_oid",
+                        column: x => x.Post_oid,
+                        principalTable: "Posts",
+                        principalColumn: "oid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_Post_oid",
+                table: "Comments",
+                column: "Post_oid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
---
+++ Projects/BackEnd_GEN/README.md
@@ -0,0 +1,11 @@
+creation du script de creeation de la base dans migration
+l'assembly doit être compilé avec les entites pour pouvoir generer le code
+
+
+
+dotnet tool install --global 
+dotnet-ef dotnet add package Microsoft.EntityFrameworkCore.Design 
+
+Mise a jour de la base de données a partir du modele
+dotnet ef migrations add InitialCreate
+dotnet ef database update
